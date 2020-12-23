3662c351749d29ef5beccafe5c5caaadf4295e44
Jake-Stokes <jakestokes01@gmail.com>
2020-12-22 20:09:16 -0500

    Add base plot script

---
+++ plot.py
@@ -0,0 +1,35 @@
+# import statements 
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+
+# set plot style
+plt.style.use('fivethirtyeight')
+
+# create the figure
+fig = plt.figure()
+
+# create a 1 dimensional subplot as an animaiton canvas
+ax1 = plt.subplot(1,1,1)
+
+def animate(i):
+    # open the data
+    plot_data = open('data.txt', 'r').read()
+    points = plot_data.split('\n')
+
+    # initialize data as empty lists
+    xs = []
+    ys = []
+
+    for point in points:
+        if len(points) > 1:
+            x, y = point.split(',')
+
+            # append values to initial lists
+            xs.append(float(x))
+            ys.append(float(y))
+    
+    ax1.clear()
+    ax1.plot(xs,ys)
+
+anim = FuncAnimation(fig, animate, interval=1000)
+plt.show()
